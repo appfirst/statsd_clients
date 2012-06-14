3b83467a6ee6a23a6aa4a027cce8c0d4a7914b58
Yangming <yangming@appfirst.com>
2012-06-14 16:37:01 -0400

    add gauge and rename some variables

--- python/statsd_client.py
+++ python/statsd_client.py
@@ -1,58 +1,73 @@
-# python_example.py
+'''
+Created on Jun 14, 2012
 
-# Steve Ivy <steveivy@gmail.com>
-# http://monkinetic.com
- 
-# this file expects local_settings.py to be in the same dir, with statsd host and port information:
-# 
-# statsd_host = 'localhost'
-# statsd_port = 8125
+@author: Steve Ivy <steveivy@gmail.com>
+@modifier: yangming <yangming@appfirst.com>
+http://www.appfirst.com
 
-# Sends statistics to the stats daemon over UDP
+python client for appfirst statsd+
+this file expects local_settings.py to be in the same dir, with statsd host and port information:
+
+statsd_host = 'localhost'
+statsd_port = 8125
+
+Sends statistics to the stats daemon over UDP
+Sends statistics to the appfirst collector over UDP
+'''
 class Statsd(object):
-    
+
     @staticmethod
-    def timing(stat, time, sample_rate=1):
+    def gauge(bucket, reading, sample_rate=1):
         """
         Log timing information
         >>> from python_example import Statsd
         >>> Statsd.timing('some.time', 500)
         """
         stats = {}
-        stats[stat] = "%d|ms" % time
+        stats[bucket] = "%d|g" % reading
         Statsd.send(stats, sample_rate)
 
     @staticmethod
-    def increment(stats, sample_rate=1):
+    def timing(bucket, elapse, sample_rate=1):
+        """
+        Log timing information
+        >>> from python_example import Statsd
+        >>> Statsd.timing('some.time', 500)
+        """
+        stats = {}
+        stats[bucket] = "%d|ms" % elapse
+        Statsd.send(stats, sample_rate)
+
+    @staticmethod
+    def increment(buckets, sample_rate=1):
         """
         Increments one or more stats counters
         >>> Statsd.increment('some.int')
         >>> Statsd.increment('some.int',0.5)
         """
-        Statsd.update_stats(stats, 1, sample_rate)
+        Statsd.update_stats(buckets, 1, sample_rate)
 
     @staticmethod
-    def decrement(stats, sample_rate=1):
+    def decrement(buckets, sample_rate=1):
         """
         Decrements one or more stats counters
         >>> Statsd.decrement('some.int')
         """
-        Statsd.update_stats(stats, -1, sample_rate)
-    
+        Statsd.update_stats(buckets, -1, sample_rate)
     @staticmethod
-    def update_stats(stats, delta=1, sampleRate=1):
+    def update_stats(buckets, delta=1, sampleRate=1):
         """
         Updates one or more stats counters by arbitrary amounts
         >>> Statsd.update_stats('some.int',10)
         """
-        if (type(stats) is not list):
-            stats = [stats]
-        data = {}
-        for stat in stats:
-            data[stat] = "%s|c" % delta
+        if (type(buckets) is not list):
+            buckets = [buckets]
+        stats = {}
+        for bucket in buckets:
+            stats[bucket] = "%s|c" % delta
 
-        Statsd.send(data, sampleRate)
-    
+        Statsd.send(stats, sampleRate)
+
     @staticmethod
     def send(data, sample_rate=1):
         """
@@ -65,9 +80,9 @@ class Statsd(object):
             addr=(host, port)
         except Error:
             exit(1)
-        
+
         sampled_data = {}
-        
+
         if(sample_rate < 1):
             import random
             if random.random() <= sample_rate:
@@ -76,7 +91,7 @@ class Statsd(object):
                     sampled_data[stat] = "%s|@%s" %(value, sample_rate)
         else:
             sampled_data=data
-        
+
         from socket import *
         udp_sock = socket(AF_INET, SOCK_DGRAM)
         try:
