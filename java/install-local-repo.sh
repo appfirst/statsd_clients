f0f9b1e614e32af2a6fc5e52556ab84f0351e2ac
Santosh Thammana <santosh@appfirst.com>
2012-11-21 18:49:34 +0000

    Fixed maven configuration

--- java/install-local-repo.sh
+++ java/install-local-repo.sh
@@ -1,12 +1,12 @@
 mvn install:install-file \
-	-Dfile=/Users/appfirst/git/statsd_clients/java/lib/jna-3.2.7.jar \
+	-Dfile=./lib/jna-3.2.7.jar \
 	-DgroupId=net.java.dev.jna \
 	-DartifactId=jna \
 	-Dversion=3.2.7 \
 	-Dpackaging=jar \
-	-DlocalRepositoryPath=/Users/appfirst/git/statsd_clients/java/lib/repo
+	-DlocalRepositoryPath=./lib/repo
 mvn install:install-file \
-	-Dfile=/Users/appfirst/git/statsd_clients/java/lib/platform-3.2.7.jar \
+	-Dfile=./lib/platform-3.2.7.jar \
 	-DgroupId=net.java.dev.jna \
 	-DartifactId=platform \
 	-Dversion=3.2.7 \
